b2054f12a99c71cdf7360af5219c177775164b39
jandonggun <donggontrannguyen@gmail.com>
2021-03-25 11:01:18 +0700

    add regis section

--- js/index.js
+++ js/index.js
@@ -45,6 +45,43 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  // count down
+  document.getElementById("submit").addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("Đăng ký tham gia");
+  });
+
+  // Set the date we're counting down to
+  var countDownDate = new Date("Apr 28, 2021 00:00:00").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("ngay").innerHTML = days;
+    document.getElementById("gio").innerHTML = hours;
+    document.getElementById("phut").innerHTML = minutes;
+    document.getElementById("giay").innerHTML = seconds;
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "Đã hết hạn đăng ký";
+    }
+  }, 1000);
+
   //   menuBar.addEventListener("click", function () {
   //     nav.classList.add("nav--animate");
   //     closeBtn.classList.add("nav__close--zoomClose");
